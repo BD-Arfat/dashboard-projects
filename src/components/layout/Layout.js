34c4cfe4ecda8bf89c650a147a4b782630eadeff
BD-Arfat <arfat1islam5162@gmail.com>
2023-10-16 22:54:01 +0600

    add

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Sidebar from '../Sidebar/Sidebar';
 
 const Layout = () => {
     return (
-        <div>
-            <div className='bg-sky-100'>slider</div>
-            <div className='bg-teal-200'>header</div>
-            <div>
-                <Outlet/>
+        <div className='flex flex-row bg-neutral-200 h-screen w-screen overflow-hidden'>
+            <Sidebar/>
+            <div className='p-4'>
+                <div className='bg-teal-200'>header</div>
+                <div>
+                    <Outlet />
+                </div>
             </div>
         </div>
     );
