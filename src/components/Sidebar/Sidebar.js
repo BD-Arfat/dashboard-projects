17cc218e92c2473160a9cd2240a08b5cf54cfdd0
BD-Arfat <arfat1islam5162@gmail.com>
2023-10-16 23:00:18 +0600

    add

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { FcBullish } from "react-icons/fc";
-import { DASHBOARD_SIDEBAR_LINKS } from '../../lib/consts/Navigation';
+import { DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS } from '../../lib/consts/Navigation';
 import { Link, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
+import { HiOutlineLogout } from 'react-icons/hi';
 
 
 const linkClass = 'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
@@ -24,7 +25,17 @@ const Sidebar = () => {
                     ))
                 }
             </div>
-            <div>bootom part</div>
+            <div className="flex flex-col gap-0.5 pt-2 border-t border-neutral-700">
+				{DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((item) => (
+					<SlidebarLink key={item.key} item={item} />
+				))}
+				<div className={classNames(linkClass, 'cursor-pointer text-red-500')}>
+					<span className="text-xl">
+						<HiOutlineLogout />
+					</span>
+					Logout
+				</div>
+			</div>
         </div>
     );
 };
