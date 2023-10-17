ce34f30209181ebf5b12195b70b2481f6e8121d5
BD-Arfat <arfat1islam5162@gmail.com>
2023-10-17 06:09:32 +0600

    add

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar/Sidebar';
+import Header from '../Header/Header';
 
 const Layout = () => {
     return (
         <div className='flex flex-row bg-neutral-200 h-screen w-screen overflow-hidden'>
-            <Sidebar/>
-            <div className='p-4'>
-                <div className='bg-teal-200'>header</div>
-                <div>
+            <Sidebar />
+            <div className='flex flex-col flex-1'>
+                <Header />
+                <div className="flex-1 p-4 min-h-0 overflow-auto">
                     <Outlet />
                 </div>
             </div>
